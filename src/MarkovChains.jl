6b50780cb593c07645a16a0e060f6eba2a8d887e
Michele Fornino <fornino.michele@gmail.com>
2020-09-29 10:43:59 -0400

    bump up to v0.1.3

--- src/MarkovChains.jl
+++ src/MarkovChains.jl
@@ -1,5 +1,29 @@
 module MarkovChains
 
-# Write your package code here.
+include("diffusionProcesses.jl")
+include("CTMC.jl")
+
+using LinearAlgebra
+using SparseArrays
+using Distributions
+
+import Base: 
+	show
+
+# Export types
+export 
+	ContinuousTimeMarkovChain,
+	ItoDiffusionProcess
+
+# Export methods
+export 
+	stationary_distribution,
+	random_sample
+
+"""
+A Julia package for simulating Markov Chains.
+
+"""
+
 
 end
